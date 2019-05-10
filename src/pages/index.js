965d8b3b67d5cf144ca0a4673904098f3da7e86d
Zach Vinless <zachvinless@gmail.com>
2019-05-09 23:24:57 -0700

    update stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ location: { origin } }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
     <h1>Hi people</h1>
@@ -14,7 +13,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <a href={`${origin}/somewhere`}>Go Somewhere</a>
   </Layout>
 )
 
