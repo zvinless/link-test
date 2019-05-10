7dd9c428ec6e7686a49195c95d7ae53102642211
Zach Vinless <zachvinless@gmail.com>
2019-05-09 23:53:25 -0700

    print out origin

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,18 @@
 import React from "react"
 
-const IndexPage = ({ location: { origin } }) => (
-  <div>
+const IndexPage = ({ location: { origin } }) => {
+  console.log(origin)
+  return (
     <div>
-      <a href="https://eloquent-bassi-2f286c.netlify.com/somewhere">Go Somewhere</a>
+      <div>
+        <a href="https://eloquent-bassi-2f286c.netlify.com/somewhere">Go Somewhere</a>
+      </div>
+      <div>
+        {/* This ends up adding /undefined between origin and /somewhere */}
+        <a href={`${origin}/somewhere`}>Go Somewhere (Undefined)</a>
+      </div>
     </div>
-    <div>
-      {/* This ends up adding /undefined between origin and /somewhere */}
-      <a href={`${origin}/somewhere`}>Go Somewhere (Undefined)</a>
-    </div>
-  </div>
-)
+  )
+}
 
 export default IndexPage
